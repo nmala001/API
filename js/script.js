dfef242c7866fcb1fe6cadbe3372c2dd3fb1b3c8
anusham21 <nmala001@odu.edu>
2018-05-28 16:15:54 -0400

    Added Google maps API

---
+++ js/script.js
@@ -0,0 +1,32 @@
+
+function loadData() {
+
+    var $body = $('body');
+    var $wikiElem = $('#wikipedia-links');
+    var $nytHeaderElem = $('#nytimes-header');
+    var $nytElem = $('#nytimes-articles');
+    var $greeting = $('#greeting');
+
+    var streetStr = $('#street').val();
+    var cityStr = $('#city').val();
+
+    var address = streetStr + ', '+ cityStr ;
+    $greeting.text('So you want to stay at'+ address + '?');
+
+    var streetviewURL = 'http://maps.googleapis.com/maps/api/streetview?size=600x300&location= '+address+' ';
+
+    $body.append('<img class="bgimg" src= "'+streetviewURL+'" >');
+
+
+    // clear out old data before new request
+    $wikiElem.text("");
+    $nytElem.text("");
+
+    // load streetview
+
+    // YOUR CODE GOES HERE!
+
+    return false;
+};
+
+$('#form-container').submit(loadData);
